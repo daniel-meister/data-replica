fdbf9dcd5cec1bd52a10fed2b7f7a88b83eb6fc6
Leonardo Sala <leonardo.sala@cern.ch>
2009-11-19 16:23:44 +0000

    added dbssql reference in the help

--- data_getNotReplicatedFiles.py
+++ data_getNotReplicatedFiles.py
@@ -5,7 +5,7 @@ from os import popen
 from optparse import OptionParser
 
 usage = """This program finds the files with no replicas around the world, and outputs one file with these files
-and another one with list of files with replicas
+and another one with list of files with replicas. It requires dbssql in the PATH
 
 USAGE: %prog  listOfLfn
 
@@ -29,7 +29,7 @@ list = open(INPUT)
 
 for f in list:
     f = f.strip('\n')
-    command = """./dbssql --input='find file,count(site) where file="""+f+""" and site!="""+MY_SITE+"""' | grep -v -e###"""
+    command = """dbssql --input='find file,count(site) where file="""+f+""" and site!="""+MY_SITE+"""' | grep -v -e###"""
     
     nReplicas = 0
     pipe = popen(command)
