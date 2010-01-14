cdd229c0b8ca26bb3d1357e80a96c0ac0b2d3c73
Leonardo Sala <leonardo.sala@cern.ch>
2010-01-14 13:11:55 +0000

    first upload

---
+++ PerfToolkit/TestBed/JRA1_RHAUTO_CHAPP_CACHE20.py
@@ -0,0 +1,127 @@
+import FWCore.ParameterSet.Config as cms
+
+#!
+#! ADJUST JET & REFERENCE PRESELECTION, RESPONSE ANALYSIS PARAMETERS
+#!
+import JetMETAnalysis.JetAnalyzers.JRA_Defaults_cff as Defaults;
+import JetMETAnalysis.JetAnalyzers.JRA_HistoDefaults_cff as Histos;
+
+Defaults.JetPtEta = cms.PSet(
+    etaMin = cms.double(-5.0),
+    etaMax = cms.double(5.0),
+    ptMin  = cms.double(1.0)
+)
+Defaults.RefPtEta = cms.PSet(
+    etaMin = cms.double(-5.0),
+    etaMax = cms.double(5.0),
+    ptMin = cms.double(10.0)
+)
+Defaults.JetResponseParameters = Histos.JetResponseParameters
+
+
+#
+# JRA defaults suitable for validation histograms
+#
+
+# pT binning
+Defaults.JetResponseParameters.binsPt = cms.vdouble(
+    10.,15.,20.,30.,45.,65.,100.,180.,250.,350.,500.,800.
+    )
+
+# eta binning
+Defaults.JetResponseParameters.binsEta = cms.vdouble(
+    0.0,1.4,2.6,3.2,4.7
+    )
+
+# absolute response histogram parameters
+Defaults.JetResponseParameters.nBinsAbsRsp = cms.uint32(200);
+Defaults.JetResponseParameters.absRspMin   = cms.double(-300);
+Defaults.JetResponseParameters.absRspMax   = cms.double(100);
+
+
+#!
+#! PROCESS
+#!
+process = cms.Process("JRA")
+
+process.load("JetMETAnalysis.JetAnalyzers.JRA_TreeDefaults_cff")
+
+
+#!
+#! INPUT
+#!
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(
+'/store/relval/CMSSW_3_3_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/MC_31X_V9-v2/0000/3A005AC9-58C8-DE11-A2F5-001A9281173A.root'
+    )
+    )
+
+
+#!
+#! SERVICES
+#!
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring('cout'),
+    cout         = cms.untracked.PSet(threshold = cms.untracked.string('WARNING'))
+)
+process.TFileService = cms.Service("TFileService",
+    fileName      = cms.string('JRA1_RHAUTO_CHAPP_CACHE20.root'),
+    closeFileFast = cms.untracked.bool(True)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+                                    cout = cms.untracked.PSet(
+    default = cms.untracked.PSet(
+    limit = cms.untracked.int32(10) ## kill all messages in the log
+    )#,
+    #FwkJob = cms.untracked.PSet(
+    #limit = cms.untracked.int32(10) ## but FwkJob category - those unlimited
+    #)
+    ),
+                                    categories = cms.untracked.vstring('FwkJob'),
+                                    destinations = cms.untracked.vstring('cout')
+                                    )
+
+process.Timing = cms.Service("Timing",
+                             useJobReport = cms.untracked.bool(True)
+                             )
+
+process.CPU = cms.Service("CPU",
+                          useJobReport = cms.untracked.bool(True),
+                          reportCPUProperties = cms.untracked.bool(True)
+                          )
+
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+                                        useJobReport = cms.untracked.bool(True)
+                                        )
+
+
+#process.AdaptorConfig = cms.Service("AdaptorConfig",   enable=cms.untracked.bool(False))
+process.source.cacheSize = cms.untracked.uint32(20*1024*1024)
+process.AdaptorConfig = cms.Service("AdaptorConfig",
+                                    cacheHint=cms.untracked.string("application-only"),
+                                    readHint=cms.untracked.string("auto-detect"))
+#process.source.cacheSize = cms.untracked.uint32(20*1024*1024)
+#process.AdaptorConfig = cms.Service("AdaptorConfig",
+#                                    cacheHint=cms.untracked.string("application-only"),
+#                                    readHint=cms.untracked.string("auto-detect")
+#                                    )
+
+#!
+#! SCHEDULE
+#!
+process.load("JetMETAnalysis.JetAnalyzers.JRA_Schedules_cff")
+process.schedule = cms.Schedule()
+process.schedule.extend(process.JRAStandardCaloJetsSchedule)
+process.schedule.extend(process.JRAStandardCaloL2L3JetsSchedule)
+# process.schedule.extend(process.JRAExtraCaloJetsSchedule)
+# process.schedule.extend(process.JRAExtraCaloL2L3JetsSchedule)
+process.schedule.extend(process.JRAStandardPFJetsSchedule)
+process.schedule.extend(process.JRAStandardPFL2L3JetsSchedule)
+# process.schedule.extend(process.JRAExtraPFJetsSchedule)
+# process.schedule.extend(process.JRAExtraPFL2L3JetsSchedule)
+# process.schedule.extend(process.JRAStandardTrkJetsSchedule)
+# process.schedule.extend(process.JRAExtraTrkJetsSchedule)
+# process.schedule.extend(process.JRAStandardJPTJetsSchedule)
---
+++ PerfToolkit/TestBed/PAT1.py
@@ -0,0 +1,94 @@
+# This is an example PAT configuration showing the usage of PAT on full sim samples
+
+# Starting with a skeleton process which gets imported with the following line
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+# load the standard PAT config
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+from PhysicsTools.PatAlgos.tools.coreTools import *
+#restrictInputToAOD(process, ['All'])
+
+# note that you can use a bunch of core tools of PAT 
+# to taylor your PAT configuration; for a few examples
+# uncomment the following lines
+#removeMCMatching(process, 'Muons')
+#removeAllPATObjectsBut(process, ['Muons'])
+#removeSpecificPATObjects(process, ['Electrons', 'Muons', 'Taus'])
+restrictInputToAOD(process)
+#removeMCMatching(process, 'Muons')
+#removeAllPATObjectsBut(process, ['Muons'])
+#removeSpecificPATObjects(process, ['Electrons', 'Muons', 'Taus'])
+
+#switch off new tau features introduced in 33X to restore 31X defaults
+# new feaures: - shrinkingConeTaus instead of fixedCone ones
+#              - TaNC discriminants attached for shrinkingConeTaus
+#              - default preselection on cleaningLayer1
+from PhysicsTools.PatAlgos.tools.tauTools import *
+switchTo31Xdefaults(process)
+
+# load the coreTools of PAT
+from PhysicsTools.PatAlgos.tools.jetTools import *
+switchJetCollection(process,
+                                        cms.InputTag('sisCone5CaloJets'),
+                                        doJTA            = True,
+                                        doBTagging       = True,
+                                        jetCorrLabel     = ('SC5','Calo'),
+                                        doType1MET       = True,
+                                        genJetCollection = cms.InputTag("sisCone5GenJets"),
+                                        doJetID      = False,
+                                        jetIdLabel   = "ak5"
+                                        )
+
+
+# let it run
+process.p = cms.Path(
+    process.patDefaultSequence
+)
+
+# In addition you usually want to change the following parameters:
+#
+#   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
+process.source.fileNames = ['/store/mc/Summer09/InclusiveMu5_Pt50/GEN-SIM-RECO/MC_31X_V3-v1/0010/8E055A2D-5587-DE11-BD4A-001F29086E74.root',
+                            '/store/mc/Summer09/InclusiveMu5_Pt50/GEN-SIM-RECO/MC_31X_V3-v1/0010/0C183AB4-5387-DE11-93CA-001CC4A6FC38.root'
+                            ]
+#process.AdaptorConfig = cms.Service("AdaptorConfig",
+#                                    tempDir=cms.untracked.string(""),
+#                                    cacheHint=cms.untracked.string("auto-detect"),
+#                                    readHint=cms.untracked.string("auto-detect")
+#                                    )
+#process.source.cacheSize = cms.untracked.uint32(20*1024*1024)
+
+#    ]         ##  (e.g. 'file:AOD.root')
+process.maxEvents.input = 1000         ##  (e.g. -1 to run on all events)
+#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+process.out.fileName = 'PAT1.root'            ##  (e.g. 'myTuple.root')
+process.out.outputCommands = cms.untracked.vstring('drop *' )
+process.options.wantSummary = False       ##  (to suppress the long output at the end of the job)    
+process.MessageLogger.cerr.threshold = 'ERROR'
+
+process.MessageLogger = cms.Service("MessageLogger",
+                                    cout = cms.untracked.PSet(
+    default = cms.untracked.PSet(
+    limit = cms.untracked.int32(10) ## kill all messages in the log
+    )#,
+    #FwkJob = cms.untracked.PSet(
+    #limit = cms.untracked.int32(10) ## but FwkJob category - those unlimited
+    #)
+            ),
+                                    categories = cms.untracked.vstring('FwkJob'),
+                                    destinations = cms.untracked.vstring('cout')
+                                                                        )
+
+process.Timing = cms.Service("Timing",
+                             useJobReport = cms.untracked.bool(True)
+                             )
+
+process.CPU = cms.Service("CPU",
+                          useJobReport = cms.untracked.bool(True),
+                          reportCPUProperties = cms.untracked.bool(True)
+                          )
+
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+                                        useJobReport = cms.untracked.bool(True)
+                                        )
+
