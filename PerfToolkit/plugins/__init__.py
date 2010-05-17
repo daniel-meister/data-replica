687cd241a03269f69121c2e70e47e8a3b2800912
Leonardo Sala <leonardo.sala@cern.ch>
2010-05-17 15:42:19 +0000

    *** empty log message ***

---
+++ PerfToolkit/plugins/__init__.py
@@ -0,0 +1,5 @@
+from cpt_CMSSW_plugin import *
+from cpt_SYS_plugin import *                                                                                                                                                                                                   
+from cpt_CRAB_plugin import *                                                                                                                                                                                                   
+from cpt_CMSSWCRAB_plugin import *                                                                                                                                                                                                   
+
