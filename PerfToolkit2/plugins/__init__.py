815f38b25fd716b056bacdd76e705cae93d39213
Leonardo Sala <leonardo.sala@cern.ch>
2011-09-14 12:21:16 +0000

    revisited version by L.Sala and M-.A. Buchmann, devel version+small modifications. Just a first upload

---
+++ PerfToolkit2/plugins/__init__.py
@@ -0,0 +1,5 @@
+from CMSSW_plugin import *
+from SYS_plugin import *                                                                                                                                                                                                   
+from CRAB_plugin import *                                                                                                                                                                                                   
+from CMSSWCRAB_plugin import *                                                                                                                                                                                                   
+
